fe36878624302e466425d336b53506eec76c12bb
Greeshmanreddy147 <mgreeshmanreddy147@gmail.com>
2025-08-19 17:13:53 +0000

    Update duties.py

--- duties.py
+++ duties.py
@@ -52,7 +52,6 @@ def changelog(ctx: Context) -> None:
     Parameters:
         ctx: The context instance (passed automatically).
     """
-
     ctx.run(git_changelog, args=[[]], title="Updating changelog")
 
 
