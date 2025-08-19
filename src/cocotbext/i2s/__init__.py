9e7311e9567be9d29824e4901b9cb60c487ef2ec
Greeshmanreddy147 <mgreeshmanreddy147@gmail.com>
2025-08-19 18:10:33 +0000

    Update __init__.py

--- src/cocotbext/i2s/__init__.py
+++ src/cocotbext/i2s/__init__.py
@@ -1,6 +1,13 @@
+
+
+"""I2S cocotb extension package.
+
+This package provides I2S (Inter-IC Sound) bus models, drivers,
+and configuration utilities for use with cocotb-based testbenches.
+"""
+
 from .bus import I2sBus
 from .driver import I2sDriver
 from .config import I2sConfig
 
 __all__ = ["I2sBus", "I2sConfig", "I2sDriver"]
-
