39c0690355d8a4d3e44e631dd5c05f0a95b5f0e1
Greeshmanreddy147 <mgreeshmanreddy147@gmail.com>
2025-08-19 17:33:43 +0000

    Update __init__.py

--- src/cocotbext/i2s/__init__.py
+++ src/cocotbext/i2s/__init__.py
@@ -2,4 +2,5 @@ from .bus import I2sBus
 from .driver import I2sDriver
 from .config import I2sConfig
 
-__all__ = ["I2sBus", "I2sDriver", "I2sConfig"]
+__all__ = ["I2sBus", "I2sConfig", "I2sDriver"]
+
